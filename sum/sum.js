f449fd13ed59463fcd661241c961d0bd95c77e1d
Frank <isfran0628@gamil.com>
2021-05-22 17:51:58 +0800

    javascript

---
+++ sum/sum.js
@@ -0,0 +1,13 @@
+console.log ("hello");
+
+function sum(n){
+    let sum=0;
+    for (let i=1; i<=n; i++){
+        sum+=i;
+    }
+    return sum;
+}
+console.log(sum(1));
+console.log(sum(2));
+console.log(sum(10));
+console.log(sum(10000));
