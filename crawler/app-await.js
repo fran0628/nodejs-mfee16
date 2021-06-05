87595edb0c18b15a5b10bb6cba56abb676bc08fa
Frank <isfran0628@gamil.com>
2021-06-05 11:21:56 +0800

    add app-await.js

---
+++ crawler/app-await.js
@@ -0,0 +1,44 @@
+const axios = require("axios");
+const fs = require("fs"); // fs=file system
+const moment = require("moment");  // 抓每天的日期
+
+function readFilePromise() {
+return new Promise((resolve, reject) => {
+    fs.readFile("stock.txt", "utf8", (err, data) => {
+        if (err){
+        reject(err);
+        }
+        resolve(data);
+    });
+});
+}
+
+//async function 
+//try catch
+(async function () {
+    try {
+      // await 回來就是 resolve
+      let stockCode = await readFilePromise();
+      let response = await axios.get(
+        "https://www.twse.com.tw/exchangeReport/STOCK_DAY",
+        {
+          params: {
+            response: "json",
+            date: moment().format("YYYYMMDD"),
+            stockNo: stockCode,
+          },
+        }
+      );
+      if (response.data.stat === "OK") {
+        console.log(response.data.date);
+        console.log(response.data.title);
+      } else {
+        // TODO 應該要處理查不回來
+      }
+    } catch (err) {
+      console.error(err);
+      // TODO
+      // 通知管理員來處理
+      // 過幾分鐘後重試
+    }
+  })();
