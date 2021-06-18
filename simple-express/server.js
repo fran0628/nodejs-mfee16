2d5052d6cb5be470cc4edab09b2ee461e0059125
Frank <isfran0628@gamil.com>
2021-06-18 15:17:32 +0800

    add public / images / javascript / style

--- simple-express/server.js
+++ simple-express/server.js
@@ -2,6 +2,11 @@ const express = require("express"); //導入express package
 
 let app = express(); //利用express 建立一個 exp applicaiton app
 
+//可以指定一個或多個目錄是靜態資源目錄
+//自動幫你為public裡面的檔案建立路由
+app.use(express.static("public"));
+
+
 app.use(function (req, res ,next){
     let current = new Date();
     console.log (`visitor is here ${current}`);
